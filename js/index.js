ae4e88434c36f522e06cda51443da620ff9c2240
Steve Lanier <stevekentlanier@gmail.com>
2019-03-04 14:33:52 -0800

    MVP in progress

--- js/index.js
+++ js/index.js
@@ -14,16 +14,16 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "h4-1":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "h4-2":"About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "h4-3":"Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "h4-4":"Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "h4-5":"Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
@@ -40,3 +40,45 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Nav
+let nav = document.querySelectorAll('a');
+nav[0].textContent = "Services";
+nav[1].textContent = "Product";
+nav[2].textContent = "Vision";
+nav[3].textContent = "Features";
+nav[4].textContent = "About";
+nav[5].textContent = "Contact";
+console.log(nav[0]);
+
+//ANUBHAV'S CODE
+let counter = 1;
+nav.forEach(currentItem => {
+  currentItem.textContent = siteContent['nav'][`nav-item-${counter}`];
+  counter++
+})
+
+//CTA
+let ctaText = document.getElementsByTagName("h1");
+ctaText[0].textContent = "DOM is awesome!"
+console.log(ctaText[0]);
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let btnText = document.getElementsByTagName("button");
+btnText[0].textContent = siteContent["cta"]["button"];
+console.log(siteContent["cta"]["button"]);
+console.log(btnText);
+
+//Main Content
+let allh4 = document.querySelectorAll("h4");
+let counter2 = 1;
+allh4.forEach(currentItem => {
+  currentItem.textContent = siteContent['main-content'][`h4-${counter2}`];
+  counter2++
+})
+
+//Contact
+
+//Footer
